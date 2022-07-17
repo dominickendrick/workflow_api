74ce2c3d90bf273a98fffe7e86ffb36a965a8a93
Dominic Kendrick <dom@dominickendrick.co.uk>
2022-07-17 16:46:59 +0100

    add some input validation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use std::fmt::Display;
 use std::fs;
 
-use std::error::Error;
-use rocket::State;
-use rocket::tokio::sync::Mutex;
-use rocket::serde::json::{Json, Value, json};
-use rocket::serde::{Serialize, Deserialize};
+use rocket::request::FromParam;
+use rocket::serde::json::{json, Json, Value};
+use rocket::serde::{Deserialize, Serialize};
 
 // The type to represent the ID of a message.
 type Id = usize;
-
-// We're going to store all of the messages here. No need for a DB.
-type CardList = Mutex<Vec<String>>;
-type Cards<'r> = &'r State<CardList>;
 
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -25,16 +21,39 @@ struct Card {
     editor: String,
     message: String,
 }
+#[derive(Debug)]
+struct Suffix<'r>(&'r str);
 
-#[get("/", format = "html")]
-fn get() -> Json<Vec<Card>> {
+impl<'r> Display for Suffix<'r> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
-    let filename = "src/data/fixtures.json";
+//Adding some input parsing in so we can make sure we are only reading files from known values
+//This also does not protect from injection attacks
+impl<'r> FromParam<'r> for Suffix<'r> {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        if param == "1" || param == "2" {
+            Ok(Suffix(param))
+        } else {
+            Err("You can only pass a 1 or 2 in here !")
+        }
+    }
+}
+
+#[get("/<suffix>")]
+fn get_suffix(suffix: Suffix) -> Json<Vec<Card>> {
+    let filename = format!("src/data/fixture{}.json", suffix.0);
+
     //load file from disk ! This is obvs not the best thing to do for peformance ...
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(&filename)
+        .expect(format!("This failed looking for {} ", &filename).as_str());
 
     //parse the input json
+    //TODO: handle parse errors correctly
     let cards: Vec<Card> = rocket::serde::json::from_str(contents.as_str()).unwrap();
 
     //serialise back to json in response
@@ -51,17 +70,13 @@ fn not_found() -> Value {
 
 pub fn stage() -> rocket::fairing::AdHoc {
     rocket::fairing::AdHoc::on_ignite("JSON", |rocket| async {
-        rocket.mount("/cards", routes![get])
+        rocket
+            .mount("/cards", routes![get_suffix])
             .register("/cards", catchers![not_found])
-            .manage(CardList::new(vec![]))
     })
 }
 
-
-
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .attach(stage())
+    rocket::build().attach(stage())
 }
-
